d10ccbc7304f86048da827f3fff83d18024328cd
Louis Jackowski-Ashley <louis.jackowskiashley@simpplr.com>
2022-11-23 14:56:42 +0000

    Working with dev

--- index.ts
+++ index.ts
@@ -1,5 +1,39 @@
 import { Kafka } from "kafkajs";
 
-async function run() {}
+const TOPIC = "CI_Campaign_Moderation_Dev";
+const FROM_BEGINNING = true;
+
+const DEV = [
+  "b-1.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
+  "b-2.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
+  "b-3.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
+];
+
+async function run() {
+  const kafka = new Kafka({
+    ssl: true,
+    brokers: DEV,
+    logLevel: 2,
+  });
+
+  const consumer = kafka.consumer({ groupId: "louis-local" });
+  await consumer.connect();
+
+  await consumer.subscribe({
+    topic: TOPIC,
+    fromBeginning: FROM_BEGINNING,
+  });
+
+  consumer.run({
+    eachMessage: async ({ message }) => {
+      const messageString = message.value?.toString();
+      if (!messageString) {
+        return;
+      }
+
+      console.log(JSON.stringify(JSON.parse(messageString), null, 2));
+    },
+  });
+}
 
 run();
