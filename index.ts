8d9624bb33a7dd28538b85d9305afac5e5cb4d5a
Ian Gilham <igilham@users.noreply.github.com>
2022-12-09 14:46:17 +0000

    feat: add CLI options parsing

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { IHeaders, Kafka } from "kafkajs";
+import { program } from "commander";
+import { exit } from "process";
 
-const TOPIC = "CI_Campaign_Item_Moderation_Dev";
-const FROM_BEGINNING = true;
+const DEFAULT_GROUP_ID = "local";
 
-const DEV = [
+const DEV_BROKERS = [
   "b-1.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
   "b-2.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
   "b-3.smtip-kafka-cluster-w.h52s5m.c14.kafka.us-east-1.amazonaws.com:9094",
@@ -27,35 +28,68 @@ const getHeaders = (
   return headers;
 };
 
-async function run() {
+async function main() {
+  program
+    .name("kafka consumer")
+    .description("Subscribes to a kafka topic and prints out messages")
+    .version("0.0.1")
+    .option(
+      "-b, --from-beginning",
+      "start reading from the oldest available message",
+      false
+    )
+    .option("-t, --topic <string>", "topic to read from")
+    .option(
+      "-g, --group-id <string>",
+      "group ID of the subscriber",
+      DEFAULT_GROUP_ID
+    );
+  program.parse();
+  const options = program.opts();
+
+  if (!options.topic) {
+    console.error("topic not specified");
+    exit(1);
+  }
+
   const kafka = new Kafka({
     ssl: true,
-    brokers: DEV,
+    brokers: DEV_BROKERS,
     logLevel: 2,
   });
 
-  const consumer = kafka.consumer({ groupId: "ian-gilham-local" });
+  const consumer = kafka.consumer({
+    groupId: options.groupId,
+  });
   await consumer.connect();
 
   await consumer.subscribe({
-    topic: TOPIC,
-    fromBeginning: FROM_BEGINNING,
+    topic: options.topic,
+    fromBeginning: options.fromBeginning ?? false,
   });
 
-  consumer.run({
+  await consumer.run({
+    autoCommit: false,
     eachMessage: async ({ message }) => {
-      const messageString = message.value?.toString() ?? '';
-      if (!messageString) {
-        console.log('empty message');
+      const messageString = message.value?.toString() ?? "";
+      if (messageString.length === 0) {
+        console.log("empty message");
+        return;
       }
       const headers = getHeaders(message.headers);
 
-      console.log(JSON.stringify({
-        headers,
-        value: JSON.parse(messageString)
-      }, null, 2));
+      console.log(
+        JSON.stringify(
+          {
+            headers,
+            value: JSON.parse(messageString),
+          },
+          null,
+          2
+        )
+      );
     },
   });
 }
 
-run();
+main();
